af25c9af18704720125d361ea726cea5d1e0f9e1
Tony Chan <tony.chan@sciteline.com>
2023-11-14 22:39:33 -0500

    Connect do db!!!

--- src/main/java/com/accountable/repositories/QuestionRepository.java
+++ src/main/java/com/accountable/repositories/QuestionRepository.java
@@ -1,16 +1,15 @@
 package com.accountable.repositories;
 
 import com.accountable.entity.Question;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-//
-//@Repository
-//public interface QuestionRepository extends JpaRepository<Question, UUID> {
-//    @Query(
-//            nativeQuery = true,
-//            value ="SELECT e FROM YourEntity e ORDER BY RANDOM()")
-//    Question getRandomQuestion();
-//}
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, UUID> {
+  //  @Query(nativeQuery = true, value = "SELECT e FROM YourEntity e ORDER BY RANDOM()")
+  //  Question getRandomQuestion();
+
+  //  @Query(nativeQuery = true, value = "SELECT * FROM questions ORDER BY RANDOM() LIMIT 1")
+  //  Question getRandomQuestion();
+}
--- src/main/java/com/accountable/services/QuestionService.java
+++ src/main/java/com/accountable/services/QuestionService.java
@@ -1,19 +1,32 @@
 package com.accountable.services;
 
 import com.accountable.entity.Question;
-// import com.accountable.repositories.QuestionRepository;
+import com.accountable.repositories.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
-    // private final QuestionRepository questionRepository;
+  private final QuestionRepository questionRepository;
 
-    public Question getRandQuestion() {
-        // return questionRepository.getRandomQuestion();
-        return null;
-    }
+  public Question getRandQuestion() {
+    // return questionRepository.getRandomQuestion();
+    return null;
+  }
+
+  public Question create(Question question) {
+    //    // TODO: Figure out how to handle exception with error code
+    //    try {
+    //      if (question.getQuestionText().isBlank()) {
+    //        throw new InvalidObjectException("Question text cannot be empty");
+    //      }
+    //    } catch (Exception e) {
+    //      log.info("Question text cannot be empty");
+    //    }
+    //
+    //    question.setIsActive(Boolean.TRUE);
+    //    // return questionRepository.saveAndFlush(question);
+    return null;
+  }
 }
